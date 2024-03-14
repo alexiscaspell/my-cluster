5b703557b95fc53e011c419e87c1178375905c02
alexiscaspell <alexiscaspell@gmail.com>
2024-03-14 19:12:08 -0300

    Fix create cluster config

--- bash/create_cluster_config.sh
+++ bash/create_cluster_config.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-# Content to write to pepe.yml
-content='write-kubeconfig-mode: "0644"
+. ./environment.sh
+
+content="write-kubeconfig-mode: \"0644\"
 tls-san:
-  - "tostado.theworkpc.com"
-cluster-init: true'
+  - \"$MASTER_K3S_HOST\"
+cluster-init: true"
 
-# Specify the file name
-file_name="/etc/rancher/k3s/config.yaml"
+file_name="config.yaml"
+# file_name="/etc/rancher/k3s/config.yaml"
 
-# Write content to the file
 echo "$content" > "$file_name"
 
-# Display a message
 echo "Content has been written to $file_name"
